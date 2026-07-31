98a4c6325c0bec1280f29a2745977402a6b2c798
Saumyadip Das <150571773+saumyadip-das-2003@users.noreply.github.com>
2026-08-01 02:09:09 +0600

    3

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "picsum.photos"
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io"
       }
     ]
   }
